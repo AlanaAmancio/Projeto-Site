c94f84b0ee4a832cc4a13bfd913620757e3b375b
Ala Amancio <alanalgm@gmail.com>
2023-12-07 15:19:05 -0300

    new commit

---
+++ html/meus exercícios/001/treino.js
@@ -0,0 +1,21 @@
+let podedeouro = 'Pote de ouro';
+let potedemana = 'Pote de Mana';
+let potesurpresa = 'Pote Surpresa';
+let potemisterioso = 'Pote Misteriso';
+console.log(
+  'No fim da caverna tem:' +
+    podedeouro +
+    ' - ' +
+    potedemana +
+    ' - ' +
+    potemisterioso
+)
+
+let nome = "Dona vovo"
+let idade = 112
+let telefone = "98 988359103"
+let cpf = "608.353.513-19"
+let endereco = "rua ver. odma rodrigues"
+let pensao = true
+
+let resu
